cf8dcfa56183f6f39fd6dd43bf55158aeb79edda
Y27A <yousifalbalooshi@gmail.com>
2026-05-08 05:36:51 +0300

    fix: redirect to /builder after sign-in and sign-up

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -5,7 +5,7 @@ export default function SignInPage() {
   usePageTitle("Sign in");
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-8rem)] py-10">
-      <SignIn routing="path" path={`${import.meta.env.BASE_URL}sign-in`} signUpUrl={`${import.meta.env.BASE_URL}sign-up`} />
+      <SignIn routing="path" path={`${import.meta.env.BASE_URL}sign-in`} signUpUrl={`${import.meta.env.BASE_URL}sign-up`} afterSignInUrl={`${import.meta.env.BASE_URL}builder`} />
     </div>
   );
 }
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -5,7 +5,7 @@ export default function SignUpPage() {
   usePageTitle("Sign up");
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-8rem)] py-10">
-      <SignUp routing="path" path={`${import.meta.env.BASE_URL}sign-up`} signInUrl={`${import.meta.env.BASE_URL}sign-in`} />
+      <SignUp routing="path" path={`${import.meta.env.BASE_URL}sign-up`} signInUrl={`${import.meta.env.BASE_URL}sign-in`} afterSignUpUrl={`${import.meta.env.BASE_URL}builder`} />
     </div>
   );
 }
